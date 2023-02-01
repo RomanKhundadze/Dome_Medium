26f0b605ad48d2d1b397a4d1dc869b9030124645
Roman Khundadze <77892691+WachsamesWiesel@users.noreply.github.com>
2023-02-01 15:59:15 +0100

    delete waste

--- BaseCode/SourceCode/compile.ps1
+++
@@ -1,4 +0,0 @@
-cd .\SourceCode\
-javac --module-path "D:\BBS\LF6\Theorie\Java\DOME-Medium\javafx-sdk-19.0.2.1\lib" --add-modules javafx.controls,javafx.fxml -g main.java UIController.java Playtime.java Medium.java CD.java DVD.java Schallplatte.java LP.java Maxi.java Datenbank.java
-java --module-path "D:\BBS\LF6\Theorie\Java\DOME-Medium\javafx-sdk-19.0.2.1\lib" --add-modules javafx.controls main
-cd ..
